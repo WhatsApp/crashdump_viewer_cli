5175480f87a0bbcf97a81d47a31f33cd9546ac81
Peixian Wang <pxw@meta.com>
2025-02-15 20:40:59 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use std::io;
+
+use ratatui::{backend::CrosstermBackend, Terminal};
+
+use crate::{
+    app::{App, AppResult},
+    event::{Event, EventHandler},
+    handler::handle_key_events,
+    tui::Tui,
+};
+
+pub mod app;
+pub mod event;
+pub mod handler;
+pub mod tui;
+pub mod ui;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Action to perform. Should be one of "tui", "json"
+    /// Default is "tui", which launches the TUI
+    /// "json" will print a JSON representation
+    #[arg(short, long, default_value_t = String::from("tui"))]
+    action: String,
+
+    /// Path to the crash dump
+    #[arg(required = true)]
+    filepath: String
+}
+
+#[tokio::main]
+async fn main() -> AppResult<()> {
+    let args = Args::parse();
+    
+    if args.action == "tui" {
+        // Create an application.
+        let mut app = App::new();
+
+        // Initialize the terminal user interface.
+        let backend = CrosstermBackend::new(io::stdout());
+        let terminal = Terminal::new(backend)?;
+        let events = EventHandler::new(250);
+        let mut tui = Tui::new(terminal, events);
+        tui.init()?;
+
+        // Start the main loop.
+        while app.running {
+            // Render the user interface.
+            tui.draw(&mut app)?;
+            // Handle events.
+            match tui.events.next().await? {
+                Event::Tick => app.tick(),
+                Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
+                Event::Mouse(_) => {}
+                Event::Resize(_, _) => {}
+            }
+        }
+
+        // Exit the user interface.
+        tui.exit()?;
+    }
+    else if args.action == "json" {
+        println!("JSON representation of the app state");
+    }
+    else {
+        println!("Invalid action: {}", args.action);
+    }
+
+    Ok(())
+}
---
+++ src/parser.rs
@@ -0,0 +1,90 @@
+use std::fs::{File, OpenOptions};
+use std::io;
+use std::path::Path;
+use rayon::prelude::*;
+use std::ffi::OsStr;
+use std::path::PathBuf;
+use std::collections::HashMap;
+use std::io::Read;
+
+
+pub struct CDParser {
+    //file: File,
+    // mmap: Mmap,
+    filepath: PathBuf,
+    filename: String,
+    crash_dump_sections: CrashDumpSections,
+}
+
+// sections for an erlang crash dump
+// each major section starts with a header of "^=.*(:.*)[0-1]\n"
+// Example
+// =erl_crash_dump:0.5
+// Sat Jan  4 19:32:02 2025
+// Slogan: forced_dump
+// System version: Erlang/OTP 27 [erts-15.2] [source] [64-bit] [smp:8:8] [ds:8:8:10] [async-threads:1] [jit]
+// Taints: 
+// Atoms: 10568
+// Calling Thread: scheduler:4
+
+pub struct CrashDumpSections {
+    pub premable: HashMap<String, String>,
+}
+
+impl CDParser {
+    pub fn new(filepath: &str) -> Result<Self, io::Error> {
+        let (filepath, filename) = Self::split_path_and_filename(filepath)?;
+        
+        
+        // need to figure out mmap later
+        // let mmap = unsafe { Mmap::map(&file)? };
+        
+        Ok(CDParser {
+            //file,
+            // mmap,
+            filepath,
+            filename,
+            crash_dump_sections: CrashDumpSections {
+                premable: HashMap::new(),
+            },
+        })
+    }
+
+    pub fn parse(&self) -> Result<Vec<String>, io::Error> {
+        let mut contents = Vec::new();
+        let realpath = self.filepath.join(&self.filename);
+        let mut file = File::open(realpath)?;
+        file.read_to_end(&mut contents)?;
+        let lines: Vec<&str> = contents.split(|c| *c == b'\n').map(|s| std::str::from_utf8(s).unwrap()).collect();
+    
+        Ok(lines.par_iter().enumerate().filter_map(|(index, line)| {
+            if line.starts_with('=') {
+                //Some((index, line.to_string()))
+                Some(format!("{} : {}", index, line.to_string()))
+            } else {
+                None
+            }
+        }).collect())
+    }
+
+    // returns the slogan, crash time, and other general information
+    pub fn get_premable(&self) -> HashMap<String, String> {
+        self.crash_dump_sections.premable.clone()
+    }
+
+    // need to list all the sections and their various counts, like unique counts for binaries, processes, etc
+    pub fn get_crash_dump_sections(&self) -> HashMap<String, String> {
+        
+    }
+    
+    fn split_path_and_filename(filepath: &str) -> Result<(PathBuf, String), io::Error> {
+        let path = Path::new(filepath);
+        let filepath = path.parent().unwrap_or(Path::new("."));
+        let filename = path.file_name().unwrap_or(OsStr::new("")).to_string_lossy();
+        Ok((filepath.to_path_buf(), filename.into_owned()))
+    }
+
+    // fn get_json() -> String {
+
+    // }
+}
