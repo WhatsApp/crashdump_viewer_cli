7a26512ac09f671db200c85dbee40d9ca0a544f4
Peixian Wang <pxw@meta.com>
2025-03-01 19:37:22 -0500

    begin address parser

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,3 +1,17 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use crate::parser::*;
 use grep::{
     regex::RegexMatcher,
@@ -56,6 +70,7 @@ impl Sink for IndexSink {
     }
 }
 
+#[derive(Default, Debug)]
 pub struct CDParser {
     //file: File,
     // mmap: Mmap,
@@ -71,6 +86,7 @@ impl CDParser {
         // need to figure out mmap later
         // let mmap = unsafe { Mmap::map(&file)? };
 
+        println!("Constructed new parser by default"))
         Ok(CDParser {
             //file,
             // mmap,
